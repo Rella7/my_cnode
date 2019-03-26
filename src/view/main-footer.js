f8fe82a1b86f2d5fb1226755ef9c439f2e9fec00
Rella7 <15631265437@163.com>
2019-03-26 15:28:19 +0800

    MainFooter

--- src/view/main-footer.js
+++ src/view/main-footer.js
@@ -6,7 +6,7 @@ export default class MainFooter extends Component {
         return <Layout.Footer style={{
             textAlign:"center"
         }}>
-            京ICP备08102442号-1 2007-2018 MIAOV.COM 版权所有
+            Made with ❤ 
         </Layout.Footer>;
     }
 }
